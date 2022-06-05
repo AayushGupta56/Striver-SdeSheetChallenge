a14cea60251768f3df03cb7df4dd4cab3e878aa8
AayushGupta56 <80825841+AayushGupta56@users.noreply.github.com>
2022-06-05 18:03:14 +0530

    Create MaximumSubarraySum.cpp

---
+++ Day1:Arrays/MaximumSubarraySum.cpp
@@ -0,0 +1,12 @@
+long long maxSubarraySum(int arr[], int n)
+{         
+    long long sum=0;long long ans=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        if(sum<0)sum=0;
+         if(sum>ans)ans=sum;
+        
+    }
+    return ans;
+    
+}
