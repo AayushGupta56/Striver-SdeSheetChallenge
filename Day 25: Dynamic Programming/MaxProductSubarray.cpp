c6b37fff693dd3ac1255f93d4e2c308a9b4e6ac8
AayushGupta56 <80825841+AayushGupta56@users.noreply.github.com>
2022-06-23 19:50:47 +0530

    Update MaxProductSubarray.cpp

--- Day 25: Dynamic Programming/MaxProductSubarray.cpp
+++ Day 25: Dynamic Programming/MaxProductSubarray.cpp
@@ -1,1 +1,15 @@
 
+int maximumProduct(vector<int> &arr, int n)
+{
+	// Write your code here
+    
+    int minele=arr[0];int maxele=arr[0];int ans=arr[0];
+    for(int i=1;i<n;i++){
+ 
+        int a=minele;int b=maxele;
+        minele=min(arr[i],min(arr[i]*a,arr[i]*b));
+         maxele=max(arr[i],max(arr[i]*a,arr[i]*b));
+         ans=max(ans,maxele);
+    }
+   return ans;
+}
