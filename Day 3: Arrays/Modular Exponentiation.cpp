e95f79301ffc8b8a0c3403072e7ace9e108047f7
AayushGupta56 <80825841+AayushGupta56@users.noreply.github.com>
2022-07-31 23:48:07 +0530

    Update Modular Exponentiation.cpp

--- Day 3: Arrays/Modular Exponentiation.cpp
+++ Day 3: Arrays/Modular Exponentiation.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h> 
+ 
 int solve(int a,int b,int m){
-	//a^b
-     if(b==1)return a;
-    long ans=solve(a,b/2);
-     if(b%2==1)return (a*(ans*ans)%m)%m;
+    //a^b
+     if(b==0)return 1;
+ long long  ans=solve(a,b/2,m);
+     if(b%2==1)return (((a%m)*(ans%m))%m*(ans%m))%m;
      else return (ans*ans)%m;
-}
-int modularExponentiation(int x, int n, int m) {
-	 int ans=1;
+} 
+ int modularExponentiation(int x, int n, int m) {
+ 
+    
+    return solve(x,n,m);
+    // 	 int ans=1;
        
-        while(n>0){
-            if(n%2==1){
-                ans=( 1LL *(ans)*(x)%m);
-                n=n-1;
-            }
-            else{
-                x=( 1LL *(x)%m*(x)%m);
-                n=n/2;
-            }
+//         while(n>0){
+//             if(n%2==1){
+//                 ans=( 1LL *(ans)*(x)%m);
+//                 n=n-1;
+//             }
+//             else{
+//                 x=( 1LL *(x)%m*(x)%m);
+//                 n=n/2;
+//             }
            
-        }
+//         }
        
-        return ans;
+//         return ans;
 }
